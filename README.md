4f27e4decd60268c35eacda357051be965eb96e8
Tim Henderson <tim.tadh@gmail.com>
2013-12-11 18:01:37 -0500

    Updated linhash to use AVL Tree. Updated README
    Signed-off-by: Tim Henderson <tim.tadh@gmail.com>

--- README.md
+++ README.md
@@ -12,16 +12,39 @@ for completeness and instructional usage.
 
 # Current Collection
 
+## Trees
+
+### Classic AVL Tree
+
+An AVL tree is a height balanced binary search tree. It is commonly taught in
+algorithms courses. The one I implemented here is from the exercise in CLRS\*:
+
+> An AVL Tree is a binary search tree that is **height balanced**: for each node
+> *x*, the heights of the left and right subtrees differ by at most 1. To
+> implement an AVL tree, we maintain an extra field in each node: *h*[x] is the
+> height of the node *x*.
+>
+> 2. To insert into an AVL tree, a node is first placed in the appropriated
+>    place in the binary search tree order. After this insertion, the tree
+>    may no longer be height blanced. Specifically, the heights of the left
+>    and right children of some node may differ by 2. Describe a procedure
+>    BALANCE(*x*), which takes a subtree rooted at *x* whole left and right
+>    children are height balanced and have heights that differ by at most
+>    2.
+
+\* : Cormen, T., Leiserson, C., Rivest, R., Stein, C. *Introduction to
+Algorithms*. McGraw-Hill Book Company. 2nd Edition. 2001.
+
 ## Hash Tables
 
 ### Classic Separate Chaining Hash Table
 
-See `hashtable/hashtable.go`. This implements the classic hash table with
+See `hashtable/hashtable.go`. An implementation of the classic hash table with
 separate chaining to handle collisions.
 
-### Linear Hash Table with BST Buckets
+### Linear Hash Table with AVL Tree Buckets
 
-See `hashtables/linhash.go`. This implement [Linear
+See `hashtables/linhash.go`. An implementation of [Linear
 Hashing](http://hackthology.com/linear-hashing.html), a technique usually used
 for secondary storage hash tables. Often employed by databases and file systems
 for hash indices. This version is mostly instructional see the
@@ -30,4 +53,19 @@ http://hackthology.com/an-in-memory-go-implementation-of-linear-hashing.html).
 If you want the "real" disk backed version you want to check my
 [file-structures](https://github.com/timtadh/file-structures) repository. See
 the `linhash` directory.
+
+### Benchmarks
+
+    $ go test -v -bench ".*" github.com/timtadh/data-structures/hashtable
+
+    BenchmarkGoMap    100000             28973 ns/op
+    BenchmarkHash      20000             76326 ns/op
+    BenchmarkMLHash    50000             73516 ns/op
+    BenchmarkLHash       500           3307005 ns/op
+
+The performance of the in memory linear hash (MLHash) is slightly improved since
+the [blog post](
+http://hackthology.com/an-in-memory-go-implementation-of-linear-hashing.html) do
+to the usage of an AVL Tree `tree/avltree.go` instead of an unbalanced binary
+search tree.
 
--- hashtable/linhash.go
+++ hashtable/linhash.go
@@ -1,7 +1,9 @@
 package hashtable
 
-import . "github.com/timtadh/data-structures/types"
-import . "github.com/timtadh/data-structures/errors"
+import (
+    . "github.com/timtadh/data-structures/types"
+    "github.com/timtadh/data-structures/tree"
+)
 
 type HashTable interface {
     Get(key Hashable) (value interface{}, err error)
@@ -25,7 +27,7 @@ type bst struct {
 }
 
 type linearhash struct {
-    table []*bst
+    table []*tree.AvlTree
     n uint
     r uint
     i uint
@@ -35,7 +37,7 @@ func NewLinearHash() *linearhash {
     N := uint(32)
     I := uint(5)
     return &linearhash{
-        table: make([]*bst, N),
+        table: make([]*tree.AvlTree, N),
         n: N,
         r: 0,
         i: I,
@@ -91,156 +93,20 @@ func (self *linearhash) Remove(key Hashable) (value interface{}, err error) {
 func (self *linearhash) split() (err error) {
     bkt_idx := self.n % (1 << (self.i - 1))
     old_bkt := self.table[bkt_idx]
-    var bkt_a, bkt_b *bst
+    var bkt_a, bkt_b *tree.AvlTree
     self.n += 1
     if self.n > (1 << self.i) {
         self.i += 1
     }
     for key, value, next := old_bkt.Iterate()(); next != nil; key, value, next = next() {
-        if self.bucket(key) == bkt_idx {
-            bkt_a, _ = bkt_a.Put(key, value)
+        if self.bucket(key.(Hashable)) == bkt_idx {
+            bkt_a, _ = bkt_a.Put(key.(Sortable), value)
         } else {
-            bkt_b, _ = bkt_b.Put(key, value)
+            bkt_b, _ = bkt_b.Put(key.(Sortable), value)
         }
     }
     self.table[bkt_idx] = bkt_a
     self.table = append(self.table, bkt_b)
     return nil
-}
-
-type BSTIterator func()(key Hashable, value interface{}, next BSTIterator)
-func (self *bst) Iterate() BSTIterator {
-    pop := func(stack []*bst) ([]*bst, *bst) {
-        if len(stack) <= 0 {
-            return stack, nil
-        } else {
-            return stack[0:len(stack)-1], stack[len(stack)-1]
-        }
-    }
-    procnode := func(stack []*bst, node *bst) []*bst {
-        if node == nil {
-            return stack
-        }
-        if node.right != nil {
-            stack = append(stack, node.right)
-        }
-        if node.left != nil {
-            stack = append(stack, node.left)
-        }
-        return stack
-    }
-    var make_iterator func(stack []*bst) BSTIterator
-    make_iterator = func(stack []*bst) BSTIterator {
-        return func()(Hashable, interface{}, BSTIterator){
-            var node *bst
-            stack, node = pop(stack)
-            if node == nil {
-                return nil, nil, nil
-            }
-            stack = procnode(stack, node)
-            return node.key, node.value, make_iterator(stack)
-        }
-    }
-    return make_iterator([]*bst{self})
-}
-
-func (self *bst) Has(key Hashable) (has bool) {
-    if self == nil {
-        return false
-    }
-    if self.key.Equals(key) {
-        return true
-    } else if key.Less(self.key) {
-        return self.left.Has(key)
-    } else {
-        return self.right.Has(key)
-    }
-}
-
-func (self *bst) Get(key Hashable) (value interface{}, err error) {
-    if self == nil {
-        return nil, Errors["not-found"](key)
-    }
-    if self.key.Equals(key) {
-        return self.value, nil
-    } else if key.Less(self.key) {
-        return self.left.Get(key)
-    } else {
-        return self.right.Get(key)
-    }
-}
-
-func (self *bst) Put(key Hashable, value interface{}) (_ *bst, updated bool) {
-    if self == nil {
-        return &bst{hash: key.Hash(), key: key, value: value}, false
-    }
-    if self.key.Equals(key) {
-        self.value = value
-        return self, true
-    }
-
-    if key.Less(self.key) {
-        self.left, updated = self.left.Put(key, value)
-    } else {
-        self.right, updated = self.right.Put(key, value)
-    }
-    return self, updated
-}
-
-func (self *bst) Remove(key Hashable) (_ *bst, value interface{}, err error) {
-    if self == nil {
-        return nil, nil, Errors["not-found"](key)
-    }
-
-    if self.key.Equals(key) {
-        if self.left != nil && self.right != nil {
-            if self.left.Size() < self.right.Size() {
-                lmd := self.right.lmd()
-                lmd.left = self.left
-                return self.right, self.value, nil
-            } else {
-                rmd := self.left.rmd()
-                rmd.right = self.right
-                return self.left, self.value, nil
-            }
-        } else if self.left == nil {
-            return self.right, self.value, nil
-        } else if self.right == nil {
-            return self.left, self.value, nil
-        } else {
-            return nil, self.value, nil
-        }
-    }
-    if key.Less(self.key) {
-        self.left, value, err = self.left.Remove(key)
-    } else {
-        self.right, value, err = self.right.Remove(key)
-    }
-    return self, value, err
-}
-
-func (self *bst) Size() int {
-    if self == nil {
-        return 0
-    }
-    return 1 + self.left.Size() + self.right.Size()
-}
-
-func (self *bst) _md(side func(*bst)*bst) (*bst) {
-    if self == nil {
-        return nil
-    } else if side(self) != nil {
-        return side(self)._md(side)
-    } else {
-        return self
-    }
-}
-
-func (self *bst) lmd() (*bst) {
-    return self._md(func(node *bst)*bst { return node.left })
-}
-
-func (self *bst) rmd() (*bst) {
-    return self._md(func(node *bst)*bst { return node.right })
 }
 
