71ba71413897ac233339557e4f93b7f688aa5708
Tim Henderson <tim.tadh@gmail.com>
2016-05-19 10:38:32 -0400

    changed the catch interface and exception types
    Signed-off-by: Tim Henderson <tim.tadh@gmail.com>

--- exc/exc_test.go
+++ exc/exc_test.go
@@ -4,11 +4,14 @@ import "testing"
 
 import "github.com/timtadh/data-structures/test"
 
+type myException struct {
+	Exception
+}
 
 func TestTry(x *testing.T) {
 	t := (*test.T)(x)
 	err := Try(func() {
-		Throwf("test", "this is a test")
+		Throwf("this is a test")
 	}).Error()
 	t.Assert(err != nil, "wanted a non nil error got %v", err)
 }
@@ -26,32 +29,55 @@ func TestTryPropogate(x *testing.T) {
 func TestTryCatch(x *testing.T) {
 	t := (*test.T)(x)
 	err := Try(func() {
-		Throwf("test", "this is a test")
-	}).Catch(&BaseException{}, func(e Exception) {
-		t.Log("Caught", e.Name())
+		Throwf("this is a test")
+	}).Catch(&Exception{}, func(e Throwable) {
+		t.Log("Caught", e)
 	}).Error()
+	t.Log(err)
+	t.Log(err == nil)
 	t.AssertNil(err)
+}
+
+func TestTryCatchMyExceptionFail(x *testing.T) {
+	t := (*test.T)(x)
+	err := Try(func() {
+		Throwf("this is a test")
+	}).Catch(&myException{}, func(e Throwable) {
+		t.Log("Caught", e)
+	}).Error()
+	t.Assert(err != nil, "should not have caught")
+}
+
+func TestTryCatchMyExceptionSuccess(x *testing.T) {
+	t := (*test.T)(x)
+	err := Try(func() {
+		Throw(&myException{*Errorf("this is a test of my Exception").Exception()})
+	}).Catch(&myException{}, func(e Throwable) {
+		t.Log("Caught", e)
+	}).Error()
+	t.Assert(err == nil, "should have caught %v", err)
 }
 
 func TestTryCatchReraise(x *testing.T) {
 	t := (*test.T)(x)
 	err := Try(func() {
-		Throwf("test", "this is a test")
-	}).Catch(&BaseException{}, func(e Exception) {
-		t.Log("Caught", e.Name())
-		Throwf("test2", "%v", e)
-	}).Error()
+		Throwf("this is a test")
+	}).Catch(&Exception{}, func(e Throwable) {
+		t.Log("Caught", e)
+		Rethrow(e, Errorf("rethrow"))
+	}).Exception()
+	t.Log(err)
 	t.Assert(err != nil, "wanted a non nil error got %v", err)
-	t.Assert(err.Name() == "test2", "wanted test2 got %v", err)
+	t.Assert(len(err.Exc().Errors) == 2, "wanted 2 errors got %v", err)
 }
 
 func TestTryCatchFinally(x *testing.T) {
 	t := (*test.T)(x)
 	finally := false
 	err := Try(func() {
-		Throwf("test", "this is a test")
-	}).Catch(&BaseException{}, func(e Exception) {
-		t.Log("Caught", e.Name())
+		Throwf("this is a test")
+	}).Catch(&Exception{}, func(e Throwable) {
+		t.Log("Caught", e)
 	}).Finally(func() {
 		t.Log("finally")
 		finally = true
@@ -64,10 +90,10 @@ func TestTryCatchReraiseFinally(x *testing.T) {
 	t := (*test.T)(x)
 	finally := false
 	err := Try(func() {
-		Throwf("test", "this is a test")
-	}).Catch(&BaseException{}, func(e Exception) {
-		t.Log("Caught", e.Name())
-		Throw(e)
+		Throwf("this is a test")
+	}).Catch(&Exception{}, func(e Throwable) {
+		t.Log("Caught", e)
+		Rethrow(e, Errorf("rethrow"))
 	}).Finally(func() {
 		t.Log("finally")
 		finally = true
@@ -80,7 +106,7 @@ func TestTryFinally(x *testing.T) {
 	t := (*test.T)(x)
 	finally := false
 	err := Try(func() {
-		Throwf("test", "this is a test")
+		Throwf("this is a test")
 	}).Finally(func() {
 		t.Log("finally")
 		finally = true
--- exc/exception.go
+++ exc/exception.go
@@ -2,53 +2,137 @@ package exc
 
 import (
 	"fmt"
+	"runtime"
 	"reflect"
+	"strings"
 )
 
 import (
 	"github.com/timtadh/data-structures/errors"
 )
 
-type Exception interface {
-	Error() string
-	Name() string
+type Error struct {
+	Err   error
+	Stack []byte
 }
 
-type BaseException struct {
-	Err *errors.Error
-	Id string
+func Errorf(format string, args ...interface{}) *Error {
+	buf := make([]byte, 50000)
+	n := runtime.Stack(buf, true)
+	trace := make([]byte, n)
+	copy(trace, buf)
+	return &Error{
+		Err:  fmt.Errorf(format, args...),
+		Stack: trace,
+	}
 }
 
-func (e *BaseException) Error() string {
-	return fmt.Sprintf("Exception: %v\n%v", e.Id, e.Err.Error())
+func FromError(err error) *Error {
+	buf := make([]byte, 50000)
+	n := runtime.Stack(buf, true)
+	trace := make([]byte, n)
+	copy(trace, buf)
+	return &Error{
+		Err: err,
+		Stack: trace,
+	}
 }
 
-func (e *BaseException) String() string {
+func (e *Error) Error() string {
+	if e == nil {
+		return "Error <nil>"
+	} else {
+		return fmt.Sprintf("%v\n\n%s", e.Err, string(e.Stack))
+	}
+}
+
+func (e *Error) String() string {
 	return e.Error()
 }
 
-func (e *BaseException) Name() string {
-	return e.Id
+func (e *Error) Exception() *Exception {
+	return &Exception{[]*Error{e}}
 }
 
-func Throwf(name, format string, args ...interface{}) {
-	throw(&BaseException{
-		Err: errors.Errorf(format, args...).(*errors.Error),
-		Id: name,
-	})
+type Throwable interface {
+	Exc()              *Exception
+	Error()            string
+	Chain(e *Error)    Throwable
 }
 
-func Throw(e Exception) {
+type Exception struct {
+	Errors []*Error
+}
+
+func (e *Exception) Exc() *Exception {
+	return e
+}
+
+func (e *Exception) Error() string {
+	errs := make([]string, 0, len(e.Errors))
+	for _, err := range e.Errors {
+		errs = append(errs, err.Error())
+	}
+	return fmt.Sprintf("Exception\n\n%v\n\nEnd Exception", strings.Join(errs, "\n---\n"))
+}
+
+func (e *Exception) String() string {
+	return e.Error()
+}
+
+func (e *Exception) Chain(err *Error) Throwable {
+	e.Errors = append(e.Errors, err)
+	return e
+}
+
+func (e *Exception) Join(exc *Exception) *Exception {
+	errs := make([]*Error, 0, len(e.Errors) + len(exc.Errors))
+	errs = append(errs, e.Errors...)
+	errs = append(errs, exc.Errors...)
+	return &Exception{
+		Errors: errs,
+	}
+}
+
+type NamedException struct {
+	*Exception
+	Nomen string
+}
+
+func (e *NamedException) Name() string {
+	return e.Nomen
+}
+
+func (e *NamedException) Join(exc *Exception) *NamedException {
+	return &NamedException{
+		Exception: e.Exception.Join(exc),
+		Nomen: e.Nomen,
+	}
+}
+
+func Throwf(format string, args ...interface{}) {
+	ThrowErr(Errorf(format, args...))
+}
+
+func ThrowErr(e *Error) {
+	throw(e.Exception())
+}
+
+func Throw(e Throwable) {
 	throw(e)
 }
 
-func throw(e Exception) {
+func Rethrow(e Throwable, err *Error) {
+	throw(e.Chain(err))
+}
+
+func throw(e Throwable) {
 	panic(e)
 }
 
 type catch struct {
 	exception reflect.Type
-	catch func(Exception)
+	catch func(Throwable)
 }
 
 type Block struct{
@@ -61,7 +145,7 @@ func Try(try func()) *Block {
 	return &Block{try: try}
 }
 
-func (b *Block) Catch(exc Exception, do func(Exception)) *Block {
+func (b *Block) Catch(exc Throwable, do func(Throwable)) *Block {
 	b.catches = append(b.catches, catch{reflect.TypeOf(exc), do})
 	return b
 }
@@ -78,17 +162,32 @@ func (b *Block) Unwind() {
 	}
 }
 
-func (b *Block) Error() Exception {
-	return b.run()
+func (b *Block) Error() error {
+	err := b.run()
+	if err != nil {
+		return err
+	} else {
+		return nil
+	}
 }
 
-func (b *Block) run() (Exception) {
+func (b *Block) Exception() Throwable {
+	t := b.run()
+	if t != nil {
+		return t
+	} else {
+		return nil
+	}
+}
+
+func (b *Block) run() (Throwable) {
 	err := b.exec()
 	if err == nil {
 		return nil
 	}
 	t := reflect.TypeOf(err)
 	for _, c := range b.catches {
+		errors.Logf("DEBUG", "try catch %v with %v", t, c.exception)
 		if t.AssignableTo(c.exception) {
 			err = Try(func(){c.catch(err)}).exec()
 			break
@@ -100,11 +199,11 @@ func (b *Block) run() (Exception) {
 	return err
 }
 
-func (b *Block) exec() (err Exception) {
+func (b *Block) exec() (err Throwable) {
 	defer func() {
 		if e := recover(); e != nil {
 			switch exc := e.(type) {
-			case Exception:
+			case Throwable:
 				err = exc
 			default:
 				panic(e)
